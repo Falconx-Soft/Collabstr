73315cacce40b452e7e31a9720f1e020436a9051
mehran <mehrankhan6799@gmail>
2022-04-07 02:04:44 +0500

    join as influencer profile page form no 6 done

--- user/models.py
+++ user/models.py
@@ -1,3 +1,5 @@
+from pickle import TRUE
+from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -18,5 +20,74 @@ class JoinInfluencer(models.Model):
     full_name= models.CharField(max_length=150,default="")
     email_address= models.EmailField(max_length=300, default="")
     password= models.CharField(max_length=30,default="")
+    location= models.CharField(max_length=100, default="")
+    title_influencer= models.CharField(max_length=80,default="")
+    description_influencer=models.CharField(max_length=1000, default="")
+    gender_influencer=models.CharField(max_length=20, default="")
+    instagram_username=models.CharField(max_length=100,default="",null=True)
+    instagram_followers=models.CharField(max_length=20, default="",null=True)
+    tiktok_username=models.CharField(max_length=100,default="",null=True)
+    tiktok_followers=models.CharField(max_length=10,default="",null=True)
+    youtube_url= models.URLField(max_length=200, default="",null=True)
+    youtube_followers=models.CharField(max_length=10,default="",null=True)
+    twitter_username=models.CharField(max_length=200,default="",null=True)
+    twitter_followers= models.CharField(max_length=10, default="",null=True)
+    twitch_username=models.CharField(max_length=200, default="",null=True)
+    twitch_followers=models.CharField(max_length=20, default="",null=True)
+    website= models.URLField(max_length=200,default="",null=True)
+    lifestyle=models.BooleanField(default=False)
+    fashion=models.BooleanField(default=False)
+    beauty=models.BooleanField(default=False)
+            
+    health_fitness=models.BooleanField(default=False)
+    
+    travel=models.BooleanField(default=False)
+            
+    food_drink=models.BooleanField(default=False)
+        
+    model=models.BooleanField(default=False)
+                
+    comedy_entertainment=models.BooleanField(default=False)
+    art_photography=models.BooleanField(default=False)
+
+    music_dance=models.BooleanField(default=False)
+        
+    entrepreneur_business=models.BooleanField(default=False)
+    family_children=models.BooleanField(default=False)
+
+    animals_pets=models.BooleanField(default=False)
+    
+    athlete_sports=models.BooleanField(default=False)
+    
+    celebrity_public_pigure=models.BooleanField(default=False)
+    adventure_outdoors=models.BooleanField(default=False)
+    actor=models.BooleanField(default=False)
+                
+    education=models.BooleanField(default=False)
+            
+    gaming=models.BooleanField(default=False)
+            
+    lgbtq=models.BooleanField(default=False)
+            
+    technology=models.BooleanField(default=False)
+        
+    healthcare=models.BooleanField(default=False)
+        
+    vegan=models.BooleanField(default=False)
+                
+    cannabis=models.BooleanField(default=False)
+            
+    skilled_trades=models.BooleanField(default=False)
+    
+    automotive=models.BooleanField(default=False)
+
+
+     
+
+
+
+
+    def __str__(self):
+            return self.influencer_username
 
 
--- user/views.py
+++ user/views.py
@@ -83,11 +83,19 @@ def join_as_brand(request):
 
 def join_as_influencer(request):
 	if request.method== 'POST':
-		influencer_username= request.POST.get('influencer_username')
-		influencer= JoinInfluencer.objects.create(influencer_username= influencer_username)
-		context= {'influencer_username': influencer_username}
-		return render(request,'User/create_your_page.html', context)
+		try:
+			influencer_username= request.POST.get('influencer_username')
+			if JoinInfluencer.objects.filter(influencer_username=influencer_username):
+				messages.success(request, 'Username is taken')
+				return redirect(request, 'join_as_influencer.html')
+			else:
 
+				influencer= JoinInfluencer.objects.create(influencer_username= influencer_username)
+				influencer.save()
+				context= {'influencer_username': influencer_username}
+				return render(request,'User/create_your_page.html', context)
+		except Exception as e:
+			print(e)
 	return render(request,'User/join_as_influencer.html')
 
 
@@ -111,4 +119,111 @@ def create_your_page(request):
 
 
 def join_influencer_profile(request):
-	return render(request,'User/create_your_page.html')
+	if request.method == 'POST':
+		influencer_username_3= request.POST.get('influencer_username_3')
+		location= request.POST.get('location_influencer')
+		title_influencer= request.POST.get('title_influencer')
+		description_influencer= request.POST.get('description_influencer')
+		gender_influencer= request.POST.get('gender_influencer')
+		instagram_username= request.POST.get('instagram_username')
+		instagram_followers= request.POST.get('instagram_followers')
+		tiktok_username= request.POST.get('tiktok_username')
+		tiktok_followers= request.POST.get('tiktok_followers')
+		youtube_username= request.POST.get('youtube_username')
+		youtube_followers= request.POST.get('youtube_followers')
+		twitter_username= request.POST.get('twitter_username')
+		twitter_followers= request.POST.get('twitter_followers')
+		twitch_username= request.POST.get('twitch_username')
+		twitch_followers= request.POST.get('twitch_followers')
+		website= request.POST.get('website')
+		niches= request.POST.get('niches_val')
+
+		influencer=JoinInfluencer.objects.get(influencer_username= influencer_username_3)
+		influencer.location=location
+		influencer.title_influencer=title_influencer
+		influencer.description_influencer=description_influencer
+		influencer.gender_influencer=gender_influencer
+		influencer.instagram_username=instagram_username
+		influencer.instagram_followers=instagram_followers
+		influencer.tiktok_username=tiktok_username
+		influencer.tiktok_followers=tiktok_followers
+		influencer.youtube_url=youtube_username
+		influencer.youtube_followers=youtube_followers
+		influencer.twitter_username=twitter_username
+		influencer.twitter_followers=twitter_followers
+		influencer.twitch_username=twitch_username
+		influencer.twitch_followers=twitch_followers
+		influencer.website=website
+		if "Lifestyle" in niches:
+			influencer.lifestyle= True
+		if "Fashion" in niches:
+			influencer.fashion= True
+		if "Beauty" in niches:
+			influencer.beauty= True
+		
+		if "Health & Fitness" in niches:
+			influencer.health_fitness= True
+		if "Travel" in niches:
+			influencer.travel= True
+		if "Food & Drink" in niches:
+			influencer.food_drink= True
+		if "Model" in niches:
+			influencer.model= True
+		if "Comedy & Entertainment" in niches:
+			influencer.comedy_entertainment= True
+		if "Art & Photography" in niches:
+			influencer.art_photography= True
+		
+		if "Music & Dance" in niches:
+			influencer.music_dance= True
+		if "Entrepreneur & Business" in niches:
+			influencer.entrepreneur_business= True
+		if "Family & Children" in niches:
+			influencer.family_children= True
+		if "Animals & Pets" in niches:
+			influencer.animals_pets= True
+		if "Athlete & Sports" in niches:
+			influencer.athlete_sports= True
+		if "Celebrity & Public Figure" in niches:
+			influencer.celebrity_public_pigure= True
+		
+		if "Adventure & Outdoors" in niches:
+			influencer.adventure_outdoors= True
+		if "Actor" in niches:
+			influencer.actor= True
+		if "Education" in niches:
+			influencer.education= True
+		if "Gaming" in niches:
+			influencer.gaming= True
+		if "LGBTQ2" in niches:
+			influencer.lgbtq= True
+		if "Technology" in niches:
+			influencer.technology= True
+		
+		if "Healthcare" in niches:
+			influencer.healthcare= True
+		if "Vegan" in niches:
+			influencer.vegan= True
+		if "Cannabis" in niches:
+			influencer.food_drcannabisink= True
+		if "Skilled Trades" in niches:
+			influencer.skilled_trades= True
+		if "Automotive" in niches:
+			influencer.automotive= True
+		influencer.save()
+		print('##########################')
+		print('location_influencer',location)
+		print('**************************')
+		print('title_influencer',title_influencer)
+		print('**************************')
+		print('description_influencer',description_influencer)
+		print('**************************')
+		print('instagram_username',instagram_username)
+		print('**************************')
+		print('instagram_followers',instagram_followers)
+		print('**************************')
+		print('tiktok_username',tiktok_username)
+		print('**************************')
+		print('niches',niches)
+		return render(request,'User/join_influencer_profile.html')
+	
